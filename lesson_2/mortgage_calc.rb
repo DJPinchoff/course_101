6857fd27af376902eda201b351f6d2a8c8f7a1d1
Dave Pinchoff <dpinchoff@gmail.com>
2017-04-23 21:15:20 +0000

    Updated Mortgage Calc App

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -1,6 +1,22 @@
 def valid_number?(num_string)
   Float(num_string) rescue false
 end
+
+def calc_payment(amount, monthly_rate, duration)
+  amount * (monthly_rate / (1 - (1 + monthly_rate)**-duration))
+end
+
+def print_thinking_timer
+  i = 1
+  print "Calculating"
+  while i <= 6
+    sleep(0.25)
+    print "."
+    i += 1
+  end
+  puts
+end
+
 puts
 puts "Welcome to the Mortgage/Car Loan Payment Calculator!"
 puts "Let's calculate your monthly payment and total cost:"
@@ -32,8 +48,8 @@ loop do
   while year_duration.nil?
     print "What's the loan duration in years? >>> "
     input = gets.chomp
-    if valid_number?(input) && input.to_i > 0
-      year_duration = input.to_i
+    if valid_number?(input) && input.to_f > 0
+      year_duration = input.to_f
     else
       puts "That's not a valid number of years... Try again."
     end
@@ -41,15 +57,19 @@ loop do
   puts
   monthly_rate = annual_rate / 12
   monthly_duration = year_duration * 12
-  monthly_payment = loan_amount * (monthly_rate /
-                                  (1 - (1 + monthly_rate)**-monthly_duration))
-  puts "Your monthly payment will be $#{monthly_payment.round(2)}."
+  monthly_payment = calc_payment(loan_amount, monthly_rate, monthly_duration)
+  print_thinking_timer
+  puts "Your monthly payment will be $#{monthly_payment.round(2)}"
   total_cost = (monthly_payment * monthly_duration).round(2)
-  puts "The total cost of your loan will be $#{total_cost}."
+  puts "The total cost of your loan will be $#{total_cost}"
   puts
   puts "Thank you for using the Mortgage/Car Payment Calculator!"
-  print "Would you like to use it again? Type 'Y' or 'N'. >>> "
-  input = gets.chomp
+  loop do
+    print "Would you like to use it again? Type 'Y' or 'N'. >>> "
+    input = gets.chomp
+    if input.downcase == 'n' || input.downcase == 'y' then break end
+    puts "Please type 'Y' or 'N'."
+  end
   if input.downcase == 'n'
     puts "Goodbye!"
     break
