78e4d9e718c2f9f1e49df320de205804c58daa39
Dave Pinchoff <dpinchoff@gmail.com>
2017-04-24 12:44:52 +0000

    mortgage_calc updated for readability_02

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -9,7 +9,7 @@ def calc_payment(amount, monthly_rate, duration)
   amount * (monthly_rate / (1 - (1 + monthly_rate)**-duration))
 end
 
-def print_thinking_timer
+def print_calculating_timer
   index = 1
   print "Calculating"
   while index <= 6
@@ -24,16 +24,17 @@ def clear_screen
   system('clear') || system('cls')
 end
 
-clear_screen
-puts "Welcome to the Mortgage/Car Loan Payment Calculator!"
-puts "Let's calculate your monthly payment and total cost:"
-puts
-
 loop do
+  clear_screen
+  puts "Welcome to the Mortgage/Car Loan Payment Calculator!"
+  puts "Let's calculate your monthly payment and total cost:"
+  puts
+
   loan_amount = nil
   while loan_amount.nil?
     print "What's the amount of the loan? >>> $"
     input = gets.chomp.tr(",", "_")
+
     if valid_number?(input) && input.to_f > 0
       loan_amount = input.to_f
     else
@@ -44,7 +45,8 @@ loop do
   annual_rate = nil
   while annual_rate.nil?
     print "What's the Annual Percentage Rate? >>> "
-    input = gets.chomp('%')
+    input = gets.chomp
+    input.chomp!('%')
 
     if valid_number?(input) && input.to_f > 0
       annual_rate = input.to_f / 100
@@ -57,6 +59,7 @@ loop do
   while year_duration.nil?
     print "What's the loan duration in years? >>> "
     input = gets.chomp
+
     if valid_number?(input) && input.to_f > 0
       year_duration = input.to_f
     else
@@ -64,22 +67,22 @@ loop do
     end
   end
 
-  puts
   monthly_rate = annual_rate / 12
   monthly_duration = year_duration * 12
   monthly_payment = calc_payment(loan_amount, monthly_rate, monthly_duration)
   total_cost = (monthly_payment * monthly_duration).round(2)
-  print_thinking_timer
+  print_calculating_timer
 
   puts "Your monthly payment will be $#{monthly_payment.round(2)}"
   puts "The total cost of your loan will be $#{total_cost}"
   puts
-  puts "Thank you for using the Mortgage/Car Payment Calculator!"
+  puts "Thank you for using the Mortgage/Car Loan Payment Calculator!"
 
   loop do
     print "Would you like to use it again? Type 'Y' or 'N'. >>> "
     input = gets.chomp.downcase
-    if input == 'n' || input == 'y' then break end
+    break if input == 'n' || input == 'y'
+
     puts "Please type 'Y' or 'N'."
   end
 
